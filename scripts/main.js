e92a57260503a026283a46136fa7d40035ca5434
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-10-19 12:54:21 +0300

    Add styles for responsive

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,19 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      500: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+      },
+      768: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      }
+    }
 });
